fb5c55c5ded2610cf11d5ef392fc5d203868f201
Surendra Sharma <surendra.sharma@cdk.com>
2016-07-08 09:19:37 +0530

    Getters are added

--- Rummy.java
+++ Rummy.java
@@ -12,3 +12,20 @@ public class Deck {
 			}
 		}
 	}
+	public Card[] getDeck(){
+		return this.deck;
+	}
+	
+	public Card[] getStartingHand(){
+		Card[] startingHand = new Card[14];
+		for(int i=0; i<13; i++){
+			startingHand[i] = deck[i];
+			deck[i].setInPlay(true);
+		}
+		this.cardsInPlay = 13;
+		return startingHand;
+	}
+	
+	public Card getTopCard(){
+		this.deck[cardsInPlay].setInPlay(true);
+		
