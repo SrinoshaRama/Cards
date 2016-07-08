f4e968a85dd387c352d9f79c9298d8defd4a55af
Navateja <Navateja.Alagam@cdk.com>
2016-07-08 10:30:39 +0530

    constructor and iswinningHand method Added

--- Rummy.java
+++ Rummy.java
@@ -11,4 +11,24 @@ public class Rummy {
 		add(new Card(100));
 	}};
 	
+	public Rummy() {
+		Collections.shuffle(pack);
+		player = new Player( new ArrayList<Card>(){{
+			for (int i = 0; i < 13; i++) {
+				add(pack.get(i));
+			}
+		}});
+	}
+	
+	public ArrayList<Card> getCards() {
+		return player.cards;
+	}
+	
+	public String isWinningHand() {
+		if(!player.checkPureSequence()) {
+			return "not a winning hand";
+		}
+		return "winning Hand";	
+	}
+	
 }
